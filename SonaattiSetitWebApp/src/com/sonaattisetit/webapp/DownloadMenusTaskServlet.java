818856f4332c12db153819e1bc60323f5c797f78
vesakilp <vesakilp@localhost>
2011-11-06 21:38:26 +0200

    lis?? setti?

--- SonaattiSetitWebApp/src/com/sonaattisetit/webapp/DownloadMenusTaskServlet.java
+++ SonaattiSetitWebApp/src/com/sonaattisetit/webapp/DownloadMenusTaskServlet.java
@@ -1,7 +1,9 @@
 package com.sonaattisetit.webapp;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import javax.servlet.http.*;
 
@@ -17,18 +19,28 @@ public class DownloadMenusTaskServlet extends HttpServlet {
 			throws IOException {
 		MenuParser parser = new MenuParser();
 		parser.parseRss("http://www.sonaatti.fi/info/RSS_fin.php");
-		
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		ArrayList<Menu> menus = parser.getMenus();
+		if(menus != null){
+			Iterator<Menu> iter = menus.iterator();
+			while(iter.hasNext()){
+				Menu menu = (Menu)iter.next();
+				System.out.println(menu.getRestaurant());
+			}
+		}
+		else{
+			//TODO jotakin virheilmoa jos ei ole saatavilla ruokalistoja, onko sit ohjelmavirhe vai eikö oikeesti oo ruokaa tarjolla eri käsittelyt
+		}
+		/*DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		Query q = new Query("Menu");
 		PreparedQuery pq = datastore.prepare(q);
 		for (Entity result : pq.asIterable()) {
 			String restaurant = (String) result.getProperty("restaurant");
-			System.out.println(restaurant);
+			System.out.println(restaurant);*/
 			/*String firstName = (String) result.getProperty("firstName");
 			String lastName = (String) result.getProperty("lastName");
 			Long height = (Long) result.getProperty("height");
-			System.out.println(lastName + " " + firstName + ", " + height.toString() + " inches tall");*/
+			System.out.println(lastName + " " + firstName + ", " + height.toString() + " inches tall");
 
-		}
+		}*/
 	}
 }
--- SonaattiSetitWebApp/src/com/sonaattisetit/webapp/MenuParser.java
+++ SonaattiSetitWebApp/src/com/sonaattisetit/webapp/MenuParser.java
@@ -13,7 +13,16 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 
 public class MenuParser {
+	private ArrayList<Menu> menus;
 	
+	public ArrayList<Menu> getMenus() {
+		return menus;
+	}
+
+	public void setMenus(ArrayList<Menu> menus) {
+		this.menus = menus;
+	}
+
 	public void parseRss(String feedUrl){
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		try {
@@ -22,45 +31,50 @@ public class MenuParser {
 				boolean item = false;
 				boolean title = false;
 				boolean description = false;
-				ArrayList<String> servings;
-				DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-				Entity menu;
-				Date date = new Date();
+				ArrayList<Menu> menus = new ArrayList<Menu>();
+				Menu menu;
+				//DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+				//Entity menu;
+				//Date date = new Date();
 				
 				public void startElement(String uri, String localName,String qName, Attributes attributes) throws SAXException {
 					if(qName.toUpperCase().equals("ITEM")) item = true;
 					if(qName.toUpperCase().equals("TITLE")) title = true;
 					if(qName.toUpperCase().equals("DESCRIPTION")){
 						description = true;
-						servings = new ArrayList<String>();
+						menu = new Menu();
+						//servings = new ArrayList<String>();
 					}
 				}			
 				public void characters(char ch[], int start, int length) throws SAXException {
 					if(item && title){
-						menu = new Entity("Menu");
+						//menu = new Entity("Menu");
 						String str = new String(ch, start, length);
 						String[] strArr = str.split("\\s+");
-						menu.setProperty("date",date);
-						menu.setProperty("restaurant", strArr[0].toLowerCase());
+						//menu.setProperty("date",date);
+						//menu.setProperty("restaurant", strArr[0].toLowerCase());
+						this.menu.setRestaurant(strArr[0].toLowerCase().trim());
 					}
 					if(item && description){
 						String str = new String(ch, start, length);
 						String[] strArr = str.split(",");
 						for(int i=0;i<strArr.length;i++){
-							servings.add(strArr[i].trim());
+							menu.getDishes().add(strArr[i].trim());
 						}
-						menu.setProperty("servings", servings);
+						//menu.setProperty("servings", servings);
 					}
+					System.out.println(new String(ch, start, length));
 				}				
 				public void endElement(String uri, String localName, String qName) throws SAXException {
 					if(qName.toUpperCase().equals("ITEM")){
 						item = false;
-						datastore.put(menu);
+						menus.add(menu);
+						//datastore.put(menu);
 					}
 					if(qName.toUpperCase().equals("TITLE")) title = false;
 					if(qName.toUpperCase().equals("DESCRIPTION")) description = false;
 				}	
-			};
+			};		
 			saxParser.parse(feedUrl, handler);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block

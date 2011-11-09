aad9e2be6fff4deaa0d076f7e6df4c8b0172145f
vkilpiai <vkilpiai@LADSZD2B.descom.fi>
2011-11-09 17:03:51 +0200

    toimii datastoreen tallennus

--- SonaattiSetitWebApp/src/com/sonaattisetit/webapp/DownloadMenusTaskServlet.java
+++ SonaattiSetitWebApp/src/com/sonaattisetit/webapp/DownloadMenusTaskServlet.java
@@ -17,31 +17,36 @@ import com.google.appengine.api.datastore.Query;
 public class DownloadMenusTaskServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		MenuParser parser = new MenuParser();
+		/*MenuParser parser = new MenuParser();
 		parser.parseRss("http://www.sonaatti.fi/info/RSS_fin.php");
 		ArrayList<Menu> menus = parser.getMenus();
 		if(menus != null){
+			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+			//Entity dbMenus = new Entity("Menus");
+			//datastore.put(dbMenus);
 			Iterator<Menu> iter = menus.iterator();
 			while(iter.hasNext()){
 				Menu menu = (Menu)iter.next();
+				Entity dbMenu = new Entity("Menu",menu.getRestaurant());
+				dbMenu.setProperty("restaurant", menu.getRestaurant());
+				dbMenu.setProperty("dishes", menu.getDishes());
+				datastore.put(dbMenu);
 				//TODO tähän entityjen luonti
 				System.out.println(menu.getRestaurant()+": "+menu.getDishes().toString());
 			}
 		}
 		else{
 			//TODO jotakin virheilmoa jos ei ole saatavilla ruokalistoja, onko sit ohjelmavirhe vai eikö oikeesti oo ruokaa tarjolla eri käsittelyt
-		}
-		/*DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		}*/
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		Query q = new Query("Menu");
 		PreparedQuery pq = datastore.prepare(q);
 		for (Entity result : pq.asIterable()) {
-			String restaurant = (String) result.getProperty("restaurant");
-			System.out.println(restaurant);*/
-			/*String firstName = (String) result.getProperty("firstName");
-			String lastName = (String) result.getProperty("lastName");
-			Long height = (Long) result.getProperty("height");
-			System.out.println(lastName + " " + firstName + ", " + height.toString() + " inches tall");
-
-		}*/
+			Menu menu = new Menu();
+			menu.setRestaurant((String)result.getProperty("restaurant"));
+			//menu.setDishes((ArrayList<String>)result.getProperty("restaurant"));
+			System.out.println(menu.getRestaurant());
+			//System.out.println(menu.getDishes().toString());
+		}
 	}
 }
